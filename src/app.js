7fc69957b978c34269050d6149ceb50f765bc57a
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-03-01 11:50:10 +0000

    funcion de genrar dominio terminada, falta enlasar html

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+   //write your code here+
+   let pronoun = ['the', 'our'];
+   let adj = ['great', 'big'];
+   let noun = ['jogger', 'racoon'];
+  
+   function generadorDominio() {
+     let dominio = [];
+     for(let itp = 0; itp < pronoun.length; itp++){
+       for(let ita = 0; ita < adj.length; ita++){
+         for(let itn = 0; itn < noun.length; itn++){
+           console.log(`${pronoun[itp]}${adj[ita]}${noun[itn]}.com`) ;
+         }
+       }
+     }
+   }
+   generadorDominio();
+  
+   //  FORMA RESUMIDA
+   /* let pronoun = ['the', 'our'];
+   let adj = ['great', 'big'];
+   let noun = ['jogger', 'racoon'];
+   pronoun.forEach(p=>
+     adj.forEach(a=>
+       noun.forEach(n=>
+         console.log(p+a+n + ".com")
+       )
+     )
+   )   */
   console.log("Hello Rigo from the console!");
 };
